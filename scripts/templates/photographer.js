d9b76989cda4877ac268475669b1d4ea9a050a8a
webmaitr <webmaitr2030@gmail.com>
2024-09-09 09:49:25 +0200

    first commit

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,17 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = city + ", " + country;
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        const fee = document.createElement('p');
+        fee.textContent = price + "€/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(fee);
         return (article);
     }
     return { name, picture, getUserCardDOM }
